174262f8814201c5cf13d6c63020eb9de9e89920
Matrix30 <Matrixboy30@gmail.com>
2023-10-20 17:12:58 +0100

    finish

--- 0-main.c
+++ 0-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {7, 1, 3, 6, 11, 14, 16, 18};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
