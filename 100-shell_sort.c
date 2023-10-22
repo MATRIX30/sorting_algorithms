008c3909b54db87de7654fbaf69b2ff5351a643c
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-21 19:36:16 -0700

    implemented shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - It performs perform Shell Sort with the Knuth Sequence
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_HEADER
 /*include neccessary libraries*/
 #include <stddef.h>
+#include <stdio.h>
 
 
 /* Data structures*/
@@ -24,6 +25,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 /* print functions*/
 void print_list(const listint_t *list);
