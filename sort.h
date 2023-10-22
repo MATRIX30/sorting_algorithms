ebef7e2dd2f0582fa08b314bd643b66ea930563b
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-21 19:42:55 -0700

    implemented quick sort

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 
 /* print functions*/
