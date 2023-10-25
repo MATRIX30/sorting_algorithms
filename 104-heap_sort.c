5d32448a1450895551d8180d10a2b6d21f40d685
Matrix30 <Matrixboy30@gmail.com>
2023-10-25 03:28:58 +0100

    added skeleton for heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+* heap_sort - function to sort array using sift-down heap sort algorithm
+* @array: array to be sorted
+* @size: size of the array
+*/
+void heap_sort(int *array, size_t size)
+{
+
+}
--- sort.h
+++ sort.h
@@ -32,6 +32,9 @@ void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 
+/*not implemented*/
+void heap_sort(int *array, size_t size);
+
 /* print functions*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
