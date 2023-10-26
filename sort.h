d6170b50e92c5d07124d0dd4c4de3d39e44c1d35
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-25 18:00:44 -0700

    implemented heap sort

--- sort.h
+++ sort.h
@@ -33,8 +33,6 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-
-/*not implemented*/
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
